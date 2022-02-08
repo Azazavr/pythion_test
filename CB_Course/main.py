de72f6a1a747462c7d379861758307a3a96243a4
Yuriy <filesua@gmail.com>
2022-02-08 14:43:07 +0200

    Генератор словаря

--- CB_Course/main.py
+++ CB_Course/main.py
@@ -8,3 +8,11 @@ def test(args):
 
 
 test([1, 2, 3, 4])
+
+#квадрат для диапазона чисел
+b = [x ** 2 for x in range(1, 5)]
+print(b)
+
+#простой ренератор словаря
+b = {x: x ** 2 for x in range(1, 5)}
+print(b)
