1478a482b1c8ab1fc4b52cd3a378acc5de41e33d
Yuriy <filesua@gmail.com>
2022-02-10 09:31:49 +0200

    Добавлен метод сравнения книг по всем параметрам

--- OOP/lecture_01/dz_01.py
+++ OOP/lecture_01/dz_01.py
@@ -1,9 +1,19 @@
-class books:
+class Books:
     def __init__(self, author, name, year, genre):
         self.author = author
         self.name = name
         self.year = year
         self.genre = genre
+
+    def __str__(self):
+        return f'{self.author}, {self.name}, {self.year}, {self.genre}'
+
+    def __eq__(self, other):
+        if isinstance(other, Books):
+            return (
+                self.author == other.author and self.name == other.name and
+                self.year == other.year and self.genre == other.genre
+            )
 
     def get_author(self):
         return self.author
@@ -17,6 +27,12 @@ class books:
     def get_genre(self):
         return self.genre
 
-book_one = books(author="Уильям Шекспир", name="Гамлет", year=2011, genre="Классика")
-book_two = books(author="Максим Горький", name="На дне", year=2016, genre="Драма")
-book_three = books(author="Братья Гримм", name="Сказки", year=2016, genre="Детские")
+
+book_one = Books(author="Уильям Шекспир", name="Гамлет", year=2011, genre="Классика")
+book_two = Books(author="Максим Горький", name="На дне", year=2016, genre="Драма")
+book_three = Books(author="Братья Гримм", name="Сказки", year=2016, genre="Детские")
+book_four = Books(author="Уильям Шекспир", name="Гамлет", year=2011, genre="Классика")
+
+print(book_one == book_two)
+print(book_one == book_four)
+
